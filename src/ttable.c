b3b7c7056a08d68cd5c383f1368e28c26d803b14
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2024-02-09 10:07:08 +0000

    Wokring on implementing process

--- src/ttable.c
+++ src/ttable.c
@@ -3,9 +3,10 @@
 #include <string.h>
 #include <ctype.h>
 #include "ttable.h"
+#define maxSize 26;
 
 static char stack[];
-static int maxSize;
+static int max = maxSize;
 static int size;
 static int numOperators;
 
@@ -14,20 +15,25 @@ int main(int argc, char *argv[]) {
         printf("Invalid number of arguments!\n");
         return 1;
     }
+
     int col;
     sscanf(argv[1], "%d", &col);
-    int row = power(col, 2);
-    findOps(argv, argc);
+    int row = power(2, col);
+    char *formula = argv[2];
+    findOps(argv);
     int table[row][col];
     genTable(row, col, table);
+
+    //for (int i = 0; i < row; i++) {
+        //runFormula(table[i], formula, col);
+    //}
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
-            runFormula(table[i]);
+            //runFormula(table[i], formula, col);
             printf("%d ", table[j][i]);
         }
         printf("\n");
     }
-    printf("\n\n");
 
     return 0;
 }
@@ -51,12 +57,12 @@ void copy(int col, int current[col], const int last[col]) {
 
 void flip(int col, int last, int table[]) {
     if (table[last] == 0) {
-        table[last + 1] = 1;
-        for (int i = last; i < col; i++) {
+        table[last] = 1;
+        for (int i = last + 1; i < col; i++) {
             table[i] = 0;
         }
     } else {
-        flip(col, last, table);
+        flip(col, last - 1, table);
     }
 }
 
@@ -69,7 +75,7 @@ int power(int base, int top) {
 }
 
 void push(char element) {
-    if (size < maxSize) {
+    if (size < max) {
         if (size > 0) {
             stack[size] = element;
             size++;
@@ -89,12 +95,65 @@ void clear() {
     size = 0;
 }
 
-void runFormula(int var[], char formula[]) {
-
+void runFormula(int var[], char formula[], int col) {
+    clear();
+    for (int i = 0; i < strlen(formula); i++) {
+        if (isalpha(formula[i])) {
+            push(formula[i]);
+        } else {
+            if (formula[i] == '-') {
+                char operand = pop();
+                char result;
+                if (operand == 'i') {
+                    result = '0';
+                } else {
+                    result = '1';
+                }
+                push(result);
+            } else if (formula[i] == '>') {
+                char operand2 = pop();
+                char operand1 = pop();
+                char result;
+                if (operand1 == '0' || operand2 == '1') {
+                    result = '1';
+                } else {
+                    result = '0';
+                }
+                push(result);
+            } else {
+                char operand2 = pop();
+                char operand1 = pop();
+                char result;
+                switch (formula[i]) {
+                    case '&':
+                        result = (operand1 == '1' && operand2 == '1') ? '1' : '0';
+                        break;
+                    case '|':
+                        result = (operand1 == '1' && operand2 == '1') ? '1' : '0';
+                        break;
+                    case '=':
+                        result = (operand1 == operand2) ? '1' : '0';
+                        break;
+                    case '#':
+                        result = (operand1 != operand2) ? '1' : '0';
+                        break;
+                    default:
+                        printf("Error: Invalid Formula!\n");
+                        exit(1);
+                }
+                push(result);
+            }
+        }
+    }
+    printf("%d", var[0]);
+    for (int i = 1; i < col; i++) {
+        printf(" %d", var[i]);
+    }
+    printf(" : %s : %c\n", formula, pop());
 }
 
 void findOps(char formula[]) {
-    for (int i = 0; i < sizeof(formula) - 2; i++) {
+    for (int i = 0; formula[i] != '\0'; i++) {
         if (!isalpha(formula[i])) {
             numOperators++;
         }
