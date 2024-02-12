d487fcca68adabf19c7ddfaf4b038b8423efcc1d
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2024-02-12 11:00:08 +0000

    Added more to part 2

--- src/partTwo.c
+++ src/partTwo.c
@@ -14,20 +14,18 @@ int main() {
     printOutput(col1, row1, formula1);
     printf("Every row in the table is equal to one, or true, therein making this a tautology and\n");
     printf("the law correct.\n");
-    printf("--------------------------------------------------------------------------------------------------\n");
 
     // Run the output for question two in Part 2 (Ian & Chris's unfair game)
     printf("--------------------------------------------------------------------------------------------------\n");
     printf("To show the outcome of Ian and Chris's game we start by defining the variables\n");
     printf("We will let a be heads, b for tails. If a is correct then Chris wins and if b is\n");
-    printf("proving the law is correct.\n");
-    int col2 = 4;
+    printf("correct then Ian loses\n");
+    int col2 = 2;
     int row2 = power(2, col2);
-    char formula2[] = "ab|cd||-a-b-&c-d-&&=";
+    char formula2[] = "ab|ab&#";
     printOutput(col2, row2, formula2);
-    printf("Every row in the table is equal to one, or true, therein making this a tautology and\n");
-    printf("the law correct.\n");
-    printf("--------------------------------------------------------------------------------------------------\n");
-
+    printf("\n");
+    printf("\n");
+    //ab#ac->&
     return 0;
 }
