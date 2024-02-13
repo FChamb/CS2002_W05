aeae58865b0d3dd3873280eda66bd5c6f41f9599
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2024-02-13 17:55:29 +0000

    Finished part 2

--- src/partTwo.c
+++ src/partTwo.c
@@ -5,40 +5,37 @@
 int main() {
     // Run the output for question one in Part 2 (De Morgan's Law)
     printf("--------------------------------------------------------------------------------------------------\n");
-    printf("To prove De Morgan's Law we will insert the entire provided formula into the\n");
-    printf("./ttable generator. If the output is a tautology the formula is valid, therein\n");
-    printf("proving the law is correct.\n");
     int col1 = 4;
     int row1 = power(2, col1);
     char formula1[] = "ab|cd||-a-b-&c-d-&&=";
+    printf("Question 1: ./ttable %d %s\n", col1, formula1);
     printOutput(col1, row1, formula1);
-    printf("Every row in the table is equal to one, or true, therein making this a tautology and\n");
-    printf("the law correct.\n");
 
     // Run the output for question two in Part 2 (Ian & Chris's unfair game)
     printf("--------------------------------------------------------------------------------------------------\n");
-    printf("To show the outcome of Ian and Chris's game we start by defining the variables\n");
-    printf("We will let a be heads, b for tails. If a is correct then Chris wins and we set\n");
-    printf("that to c. If b is correct then Ian loses and we set that to d.\n");
-    int col2 = 4;
+    int col2 = 2;
     int row2 = power(2, col2);
-    char formula2[] = "ac&bd&|";
+    char formula2[] = "ab>a-b>|";
+    printf("Question 2: ./ttable %d %s\n", col2, formula2);
     printOutput(col2, row2, formula2);
     printf("\n");
     printf("\n");
-    //ab#ac->&
 
     // Run the output for question three in Part 2 (A dinner party)
     printf("--------------------------------------------------------------------------------------------------\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
     int col3 = 5;
-    int row3 = power(2, col1);
-    char formula3[] = "dc|be#be&-&ab>ed>de>&cad&>";
+    int row3 = power(2, col3);
+    char formula3[] = "dc|be#ab&ed>de>&cad&>";
+    printf("Question 3: ./ttable %d %s\n", col3, formula3);
     printOutput(col3, row3, formula3);
-    printf("\n");
-    printf("\n");
+
+    // Run the output for question four in Part 2 (Which box is it?)
+    printf("--------------------------------------------------------------------------------------------------\n");
+    int col4 = 9;
+    int row4 = power(2, col4);
+    char formula4[] = "da>ea>&fh>&ga->&ha->&hh->&aaa-#>&baa-#>&chh->&ab#c#&de#f#&gh#i#&ad-g-&b-&c-&>&da-g-&e-&f-&>&ga-d-&h-&i-&>&be-h-&a-&c-&>&eb-h-&d-&f-&>&hb-e-&g-&i-&>&cf-i-&a-&b-&>&fc-i-&d-&e-&>&ic-f-&g-&h-&>&"; //"da>ea>&fh>&ga->&ha->&hh->&aaa-|>&baa-|>&chh->&ab|c|&de|f|&gh|i|&ad-g-&b-&c-&>&da-g-&e-&f-&>&ga-d-&h-&i-&>&be-h-&a-&c-&>&eb-h-&d-&f-&>&hb-e-&g-&i-&>&cf-i-&a-&b-&>&fc-i-&d-&e-&>&ic-f-&g-&h-&>&";
+    printf("Question 4: ./ttable %d %s\n", col4, formula4);
+    printOutput(col4, row4, formula4);
 
     return 0;
 }
